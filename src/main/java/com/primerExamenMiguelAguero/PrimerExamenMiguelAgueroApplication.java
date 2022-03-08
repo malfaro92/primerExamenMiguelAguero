33b1bb16b0e63088b7f390cc106c6c049f4bda4b
malfaro92 <72321595+malfaro92@users.noreply.github.com>
2022-03-07 18:21:15 -0600

    Primer commit del examen para subirlo a github.

---
+++ src/main/java/com/primerExamenMiguelAguero/PrimerExamenMiguelAgueroApplication.java
@@ -0,0 +1,13 @@
+package com.primerExamenMiguelAguero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerExamenMiguelAgueroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerExamenMiguelAgueroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primerExamenMiguelAguero/PrimerExamenMiguelAgueroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primerExamenMiguelAguero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerExamenMiguelAgueroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
